2b9bb7c981d5bae5a8f123ac71b54c7623cfbc8a
WangBaiqi <910346765@qq.com>
2020-09-17 17:21:05 +0800

    Initial commit

---
+++ src/main/java/com/pocky/springboot/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.pocky.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * @author pocky
+ * @date 2020/09/16/0016
+ */
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
+    //想查出一些数据，在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
+    }
+}
